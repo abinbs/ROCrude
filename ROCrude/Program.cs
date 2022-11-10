2ad22bec43e3a1002d764c17680f7552fe8d692e
abinbs <abinbinusam@gmail.com>
2022-11-10 16:47:13 +0530

    Basic backend

---
+++ ROCrude/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ROCrude
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ROView cvw = new ROView();
+            bool sts = true;
+            while (sts)
+            {
+                int ch = cvw.MenuChoice();
+                switch (ch)
+                {
+                    case 1:
+                        cvw.AddROView();
+                        break;
+                    case 2:
+                        cvw.UpdateROView();
+                        break;
+                    case 3:
+                        cvw.DeleteROView();
+                        break;
+                    case 4:
+                        cvw.FindROView();
+                        break;
+                    case 5:
+                        cvw.ROSummaryView();
+                        break;
+                    case 6:
+                        sts = false;
+                        cvw = null;
+                        break;
+                    default:
+                        Console.WriteLine("\n\n******Wrong Choice*******\n\n");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ ROCrude/RO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class RO
+    {
+        public int ROID
+        {
+            get;
+            set;
+        }
+        public string CNAME
+        {
+            get;
+            set;
+        }
+
+        public string CADDRESS
+        {
+            get;
+            set;
+        }
+        public string CMAKE
+        {
+            get;
+            set;
+        }
+        public int MLGE
+        {
+            get;
+            set;
+        }
+        public override string ToString()
+        {
+            string s = "";
+            s += "\nRO ID\t:\t" + ROID;
+            s += "\nCustomer Name\t:\t" + CNAME;
+            s += "\nAddress\t\t:\t" + CADDRESS;
+            s += "\nModel\t\t:\t" + CMAKE;
+            s += "\nMileage\t:\t" + MLGE;
+            return s;
+        }
+    }
+}
